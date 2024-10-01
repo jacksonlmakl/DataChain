a7eef0f20ae1c03553aa0104c3b499fdb483cadb
Jackson Makl <127900729+jacksonlmakl@users.noreply.github.com>
2024-10-01 16:20:37 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ long_description = (here / "README.md").read_text(encoding="utf-8")
 
 setup(
     name='py-chain',
-    version='1.0',
+    version='1.1',
     packages=find_packages(),
     install_requires=[
         'requests',
