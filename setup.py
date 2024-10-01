590229013b3d648d13e126f2fc20fc974a4ace72
Jackson Makl <jackson.makl@dataiku.com>
2024-10-01 17:38:36 -0400

    rename

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ long_description = (here / "README.md").read_text(encoding="utf-8")
 
 setup(
     name='ChainEngine',
-    version='1.5',
+    version='1.6',
     packages=find_packages(),
     install_requires=[
         'requests',
