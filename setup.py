aa8b452b71894c4f6f7a0981abe2c6db9966f5de
Jackson Makl <127900729+jacksonlmakl@users.noreply.github.com>
2024-10-01 21:27:48 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ long_description = (here / "README.md").read_text(encoding="utf-8")
 
 setup(
     name='ChainEngine',
-    version='1.8',
+    version='1.9',
     packages=find_packages(),
     install_requires=[
         'requests',
